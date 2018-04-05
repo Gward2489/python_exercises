a92a676216679ab140828fe45de8800b791d79ee
Gward2489 <gww8804@gmail.com>
2018-04-05 17:40:50 -0500

    both stocks exercises complete

--- dictionaries/stocks.py
+++ dictionaries/stocks.py
@@ -1,8 +1,13 @@
 STOCK_DICT = {'GM': 'General Motors', 'CAT':'Caterpillar', 'EK':'Eastman Kodak',
-              'GE':'General Electric'}
+              'GE':'General Electric', 'NIN': 'Nintendo'}
 
 PURCHASES = [('GE', 100, '10-sep-2001', 48), ('CAT', 100, '1-apr-1999', 24),
-             ('GE', 200, '1-jul-1998', 56), ('EK', 777, '5-oct-208', 99)]
+             ('GE', 200, '1-jul-1998', 56), ('EK', 777, '5-oct-2008', 99),
+             ('NIN', 68, '8-aug-2003', 435), ('GM', 666, '9-feb-1999', 420),
+             ('GM', 606, '12-feb-1999', 520), ('NIN', 68, '8-aug-2003', 435)]
+
+print('')
+print('Purchase History:')
 
 for x in PURCHASES:
     name = ''
@@ -11,3 +16,13 @@ for x in PURCHASES:
             name = STOCK_DICT[key]
     price = str(x[1] * x[3])
     print(f'On {x[2]} ${price} worth of {name} was purchased')
+
+print('')
+print('Totals:')
+
+for key in STOCK_DICT:
+    current_total = 0
+    for x in PURCHASES:
+        if key == x[0]:
+            current_total += x[1] * x[3]
+    print(f'{key}: ${str(current_total)}')
